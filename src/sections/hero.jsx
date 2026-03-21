2c19c303945eebe82a1bad318eaefc897f2816c5
Silvia <silviaislam.eera@gmail.com>
2026-03-21 22:55:51 +0600

    update: className format

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,32 +1,49 @@
 export default function Hero () {
   return (
     <section className="hero">
-      <div className="wrapper-logo mx-auto my-0">
-        <p className="logo-wordmark text-center text-3xl sm:text-4xl md:text-5xl text-black uppercase max-w-full mx-auto p-16">
+      <div className={`
+          wrapper-logo
+          mx-auto
+          my-0
+          p-16
+        `}>
+        <p className={`
+          logo-wordmark
+          text-center
+          text-3xl
+          sm:text-4xl
+          md:text-5xl
+          text-black
+          uppercase
+          max-w-full mx-auto p-16`}>
           SEEMA & ROHAN
         </p>
       </div>
 
       <div className="hero-image relative">
 
-        <div className="text-wrapper mt-16 mb-10 w-full z-10 text-center flex flex-col items-center justify-center sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:m-0">
-          <h1 className='py-2 px-4 text-lg sm:text-2xl md:text-4xl lg:text-5xl leading-snug bg-white inline-block lg:py-5 lg:px-20 '>
-            Complimentary transportation will be provided for all guests traveling between Jan. 31st and Feb. 4th
-            <br />
-            <span
-              className={`
-              text-sm
-              md:text-xl
-              lg:text-2xl
-              `}
-              >
-              PLEASE BOOK NOW AND SECURE YOUR AIRPORT TRANSPORTATION SERVICE
-            </span>
-          </h1>
-        </div>
-
         <img src="./imgs/hero.png" alt="transport image" className="w-full"/>
+
+        <div className={`
+          text-wrapper
+          absolute 
+          bottom-0 left-0 right-0 
+          z-10
+          flex justify-center
+          px-4 pb-6 sm:pb-10 md:pb-12
+        `}>
+          <h1 className={`
+            py-2 px-4 
+            text-lg
+            sm:text-2xl md:text-4xl lg:text-5xl
+            leading-snug
+            bg-white text-center 
+            max-w-5xl lg:py-5 lg:px-20
+            `}>
+            Complimentary transportation will be provided
+          </h1>
+        </div>  
       </div>
     </section>
-  )
+  );
 }
