44638d3e80b571d3d790c8cfd6e41ec3c9c9869b
Silvia <silviaislam.eera@gmail.com>
2026-03-22 21:36:03 +0600

    update(color-pallete): add dark green

--- src/sections/form.jsx
+++ src/sections/form.jsx
@@ -273,7 +273,6 @@ export default function Form() {
 
         </div>
 
-        <VipCode />
 
         <div
           className={`
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        "black": "#1a1a1a",
+        "black": "#3a4c35",
         "white": "#ffffff",
         "gold": "#C8B568",
       }
