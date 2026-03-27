16908d2b247330055e9fb56aae3b555fe08eb8f6
daridev <darideveloper@gmail.com>
2026-03-27 14:25:03 -0600

    feat (Faqs): hotel from api

--- src/sections/faqs.jsx
+++ src/sections/faqs.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react'
 import Subtitle from '../components/subtitle'
 import FaqItem from '../components/faq-item'
+import { getHotels } from '../api/hotels'
 
 export default function Faqs() {
+  const [hotelName, setHotelName] = useState('the resort')
+
+  useEffect(() => {
+    getHotels().then(hotels => {
+      if (hotels && hotels.length > 0 && hotels[0].value !== 'other') {
+        setHotelName(hotels[0].label)
+      }
+    })
+  }, [])
+
   const faqs = [
     {
       question: "WHY SHOULD I BOOK WITH CANCUN CONCIERGE? I AM SEEING CHEAPER TRANSPORTATION RATES ONLINE.",
@@ -17,7 +29,7 @@ export default function Faqs() {
         <ul className="list-disc pl-5 space-y-2">
           <li>Personalized email transportation confirmation</li>
           <li>Meet & greet staff at the airport dedicated for Seema & Rohan's wedding</li>
-          <li>Refreshments</li>
+          <li>Bottled waters</li>
           <li>Professional and trusted drivers</li>
           <li>A single rate inclusive of all taxes and fees</li>
         </ul>
@@ -27,7 +39,7 @@ export default function Faqs() {
       question: "HOW DO I FIND CANCUN CONCIERGE ONCE I'M READY TO LEAVE THE AIRPORT?",
       answer: (
         <p>
-          Our staff will be wearing white polo shirts and khaki pants and holding a sign with Seema & Rohan's wedding. The staff will be waiting right outside the airport terminal by the pre-arranged transportation area. Our staff will escort you to your awaiting vehicle to safely and quickly get you over to the Hilton Tulum Riviera Maya
+          Our staff will be wearing white polo shirts and khaki pants and holding a sign with Seema & Rohan's wedding. The staff will be waiting right outside the airport terminal by the pre-arranged transportation area. Our staff will escort you to your awaiting vehicle to safely and quickly get you over to the {hotelName}
         </p>
       )
     },
@@ -35,7 +47,7 @@ export default function Faqs() {
       question: "HOW DO I PAY FOR MY TRANSPORTATION?",
       answer: (
         <p>
-          You can pay right here! Use your credit or debit card to finish the purchase of your transportation between the Cancun Airport and the Hilton Tulum Riviera Maya resort.
+          You can pay right here! Use your credit or debit card to finish the purchase of your transportation between the Cancun Airport and the {hotelName} resort.
         </p>
       )
     },
